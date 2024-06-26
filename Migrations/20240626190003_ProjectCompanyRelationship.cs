e3b1d0f59fe9a9c2ca2f42ce6bf40bb2558db381
Daniel Visan <dvisan15@yahoo.com>
2024-06-26 22:03:33 +0300

    first commit

---
+++ Migrations/20240626190003_ProjectCompanyRelationship.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RelationshipEnitites.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProjectCompanyRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CompanyProject",
+                columns: table => new
+                {
+                    CompaniesId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProjectsId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyProject", x => new { x.CompaniesId, x.ProjectsId });
+                    table.ForeignKey(
+                        name: "FK_CompanyProject_Companies_CompaniesId",
+                        column: x => x.CompaniesId,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CompanyProject_Projects_ProjectsId",
+                        column: x => x.ProjectsId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompanyProject_ProjectsId",
+                table: "CompanyProject",
+                column: "ProjectsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CompanyProject");
+
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TaskRelationshipsWebApi
 # TaskRelationshipsWebApi
+# TaskRelationshipsWebApi
